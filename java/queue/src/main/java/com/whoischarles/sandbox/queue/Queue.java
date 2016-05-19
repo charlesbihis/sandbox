4c21d5ee66f0b4ee7c9d54fa02f40a1271c446e3
Charles Bihis <charles@whoischarles.com>
2016-05-19 00:05:03 -0700

    Fix a bug with the implementation as well as add comments as to why this implementation sucks

--- java/queue/src/main/java/com/whoischarles/sandbox/queue/Queue.java
+++ java/queue/src/main/java/com/whoischarles/sandbox/queue/Queue.java
@@ -1,5 +1,17 @@
 package com.whoischarles.sandbox.queue;
 
+/**
+ * This is a shitty implementation.  I'll tell you why.  For this implementation of a queue, I'm using a linked-list
+ * to store the nodes.  When I enqueue, I insert at the front of the queue, and when I dequeue, I remove from the end
+ * of the queue.  Since I add at the front and I have a pointer to 'head', inserts are constant.  However, even though
+ * I have a pointer to tail, and since this isn't a doubly linked list, I have to traverse the entire list to get the
+ * node right before the tail in order to remove the tail.  This makes the dequeue operation linear.
+ *
+ * What I should do instead is enqueue by inserting at the end, and dequeue by removing from the front.  This would make
+ * both operations constant.
+ *
+ * @param <T>
+ */
 public class Queue<T> {
   protected Node<T> head;
   protected Node<T> tail;
@@ -26,7 +38,8 @@ public class Queue<T> {
 
     if (head.equals(tail)) {
       Node<T> result = tail;
-      tail = head;
+      head = null;
+      tail = null;
       size--;
       return result.getValue();
     }
@@ -39,6 +52,7 @@ public class Queue<T> {
         size--;
         return result.getValue();
       }
+      currNode = currNode.getNext();
     }
 
     return null;
--- java/queue/src/test/java/com/whoischarles/sandbox/queue/QueueTest.java
+++ java/queue/src/test/java/com/whoischarles/sandbox/queue/QueueTest.java
@@ -16,5 +16,27 @@ public class QueueTest {
     int result = queue.dequeue();
     Assert.assertEquals(12345, result);
     Assert.assertEquals(0, queue.getSize());
+
+    queue.enqueue(1);
+    queue.enqueue(2);
+    queue.enqueue(3);
+    queue.enqueue(4);
+    queue.enqueue(5);
+    Assert.assertEquals(5, queue.getSize());
+
+    result = queue.dequeue();
+    Assert.assertEquals(1, result);
+
+    result = queue.dequeue();
+    Assert.assertEquals(2, result);
+
+    result = queue.dequeue();
+    Assert.assertEquals(3, result);
+
+    result = queue.dequeue();
+    Assert.assertEquals(4, result);
+
+    result = queue.dequeue();
+    Assert.assertEquals(5, result);
   }
 }
